861c7673ed2685e2957b55d378f94f4dfaf9d666
Imam2719 <alimamuddin755@gmail.com>
2025-10-14 23:10:46 +0600

    Project Initialize

---
+++ src/main/java/com/example/Ostad_SpringBoot/OstadSpringBootApplication.java
@@ -0,0 +1,14 @@
+package com.example.Ostad_SpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//2
+@SpringBootApplication
+public class OstadSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OstadSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/Ostad_SpringBoot/OstadSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Ostad_SpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OstadSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
