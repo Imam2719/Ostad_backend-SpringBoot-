3171bca329844f52769d968e48abfb706e821e4c
Imam2719 <alimamuddin755@gmail.com>
2026-01-14 22:45:29 +0600

    Module 21

--- src/main/java/com/example/Ostad_SpringBoot/Module_21/repository/WeatherApiResponse.java
+++ src/main/java/com/example/Ostad_SpringBoot/Module_21/repository/WeatherApiResponse.java
@@ -1,4 +1,37 @@
 package com.example.Ostad_SpringBoot.Module_21.repository;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class WeatherApiResponse {
+    private Location location;
+    private Current current;
+
+    @Data
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class Location {
+        private Double lat;
+        private Double lon;
+        private String name;
+        private String region;
+        private String country;
+        private String localtime;
+    }
+
+    @Data
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class Current {
+        private Double temp_c;
+        private Double wind_kph;
+        private Double humidity;
+        private Condition condition;
+    }
+
+    @Data
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class Condition {
+        private String text;
+    }
 }
