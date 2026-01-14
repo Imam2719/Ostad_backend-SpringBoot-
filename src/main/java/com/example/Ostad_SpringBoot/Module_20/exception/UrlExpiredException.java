bc55726253c0726539344d816e9722c397997499
Imam2719 <alimamuddin755@gmail.com>
2026-01-14 22:42:49 +0600

    Module 21

--- src/main/java/com/example/Ostad_SpringBoot/Module_20/exception/UrlExpiredException.java
+++
@@ -1,7 +0,0 @@
-package com.example.Ostad_SpringBoot.Module_20.exception;
-
-public class UrlExpiredException extends RuntimeException {
-    public UrlExpiredException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/example/Ostad_SpringBoot/Module_20/exception/UrlNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.Ostad_SpringBoot.Module_20.exception;
-
-public class UrlNotFoundException extends RuntimeException {
-    public UrlNotFoundException(String message) {
-        super(message);
-    }
-}
